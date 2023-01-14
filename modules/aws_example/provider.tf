6a4fb48b56bf875e06299b2e391d7c2c48417676
GitHub Autobot <autobot@no-reply.github.com>
2023-01-14 01:04:15 +0000

    terraform checks

--- modules/aws_example/provider.tf
+++ modules/aws_example/provider.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_version = "~> 1.3"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.49"
-        }
+  required_version = "~> 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
-    profile =  var.aws_profile
+  region  = var.aws_region
+  profile = var.aws_profile
 }
